5a7ca7eef2c01cac5b6d04c2055caa846453baab
wzq <1228740123@qq.com>
2021-05-09 18:18:42 +0800

    runable

--- buildSrc/src/main/java/org/wzq/leetcode/LeetcodeHelperPlugin.java
+++ buildSrc/src/main/java/org/wzq/leetcode/LeetcodeHelperPlugin.java
@@ -1,7 +1,12 @@
 package org.wzq.leetcode;
 
+import com.ciaoshen.leetcode.helper.ProblemBuilder;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 
 public class LeetcodeHelperPlugin implements Plugin<Project> {
@@ -16,5 +21,35 @@ public class LeetcodeHelperPlugin implements Plugin<Project> {
             );
         }).setGroup(TASK_GROUP);
 
+        target.getExtensions().create("problem", Problem.class);
+
+        target.afterEvaluate(t -> {
+            Problem problem = (Problem) target.getExtensions().getByName("problem");
+            System.out.println(problem);
+            System.out.println(target.getRootDir());
+        });
+
+        target.task("create111", task -> {
+            task.doLast(t -> {
+                Problem problem = (Problem) target.getExtensions().getByName("problem");
+                String[] args = new String[]{
+                    target.getRootDir().toString(), problem.name,
+                    problem.pck, problem.util, problem.members
+                };
+                ProblemBuilder builder = new ProblemBuilder(args);
+                builder.writeTemplates();
+            });
+        });
+    }
+
+    Properties loadProblemProperties(Project project) {
+        Properties problemProperties = new Properties();
+        String path = project.absoluteProjectPath("problem.properties");
+        try {
+            problemProperties.load(new FileInputStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return problemProperties;
     }
 }
---
+++ buildSrc/src/main/java/org/wzq/leetcode/Problem.java
@@ -0,0 +1,18 @@
+package org.wzq.leetcode;
+
+public class Problem {
+    public String name;
+    public String pck;
+    public String members;
+    public String util;
+
+    @Override
+    public String toString() {
+        return "Problem{" +
+            "name='" + name + '\'' +
+            ", pck='" + pck + '\'' +
+            ", members='" + members + '\'' +
+            ", util='" + util + '\'' +
+            '}';
+    }
+}
