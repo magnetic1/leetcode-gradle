86f4389a0fcb35bb5db5e4c52daaebd0f9c94491
wzq <1228740123@qq.com>
2021-05-12 20:04:34 +0800

    xor_queries_of_a_subarray

---
+++ src/main/java/com/leetcode/xor_queries_of_a_subarray/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - xor_queries_of_a_subarray
+ */
+package com.leetcode.xor_queries_of_a_subarray;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/xor_queries_of_a_subarray/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - xor_queries_of_a_subarray
+ */
+package com.leetcode.xor_queries_of_a_subarray;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
