0228fb1f0934eb462f3ab60ecd75d5690f46da0a
wzq <1228740123@qq.com>
2021-05-13 21:33:12 +0800

    number_of_ways_to_stay_in_the_same_place_after_some_steps

---
+++ src/main/java/com/leetcode/number_of_ways_to_stay_in_the_same_place_after_some_steps/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - number_of_ways_to_stay_in_the_same_place_after_some_steps
+ */
+package com.leetcode.number_of_ways_to_stay_in_the_same_place_after_some_steps;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/number_of_ways_to_stay_in_the_same_place_after_some_steps/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - number_of_ways_to_stay_in_the_same_place_after_some_steps
+ */
+package com.leetcode.number_of_ways_to_stay_in_the_same_place_after_some_steps;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
