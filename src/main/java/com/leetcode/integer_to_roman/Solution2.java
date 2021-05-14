63beae9f9ac9b4bfd6c61ac119f85168299877f8
wzq <1228740123@qq.com>
2021-05-14 22:56:05 +0800

    integer_to_roman

--- src/main/java/com/leetcode/integer_to_roman/Solution2.java
+++ src/main/java/com/leetcode/integer_to_roman/Solution2.java
@@ -14,6 +14,26 @@ package com.leetcode.integer_to_roman;
  */
 class Solution2 implements Solution {
 
-    
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] rom = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (i < 13) {
+            if (num / values[i] > 0) {
+                sb.append(rom[i]);
+                num -= values[i];
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        System.out.println(solution.intToRoman(2569));
+    }
 }
