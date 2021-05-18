b8277ab7526d09104e93df2dadb2fddbed23df87
wzq <1228740123@qq.com>
2021-05-18 13:45:58 +0800

    count_triplets_that_can_form_two_arrays_of_equal_xor

---
+++ src/main/java/com/leetcode/count_triplets_that_can_form_two_arrays_of_equal_xor/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - count_triplets_that_can_form_two_arrays_of_equal_xor
+ */
+package com.leetcode.count_triplets_that_can_form_two_arrays_of_equal_xor;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
