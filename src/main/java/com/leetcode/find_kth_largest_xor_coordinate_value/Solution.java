13eb98d6f251b302ddf3a99015afbc5f307c3c8b
wzq <1228740123@qq.com>
2021-05-19 13:47:46 +0800

    find_kth_largest_xor_coordinate_value

---
+++ src/main/java/com/leetcode/find_kth_largest_xor_coordinate_value/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - find_kth_largest_xor_coordinate_value
+ */
+package com.leetcode.find_kth_largest_xor_coordinate_value;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
