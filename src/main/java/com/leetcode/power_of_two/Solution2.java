be99ec0479a34270ca32aed70857bfd07c12a549
wzq <1228740123@qq.com>
2021-05-31 17:03:00 +0800

    power_of_two

---
+++ src/main/java/com/leetcode/power_of_two/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - power_of_two
+ */
+package com.leetcode.power_of_two;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
