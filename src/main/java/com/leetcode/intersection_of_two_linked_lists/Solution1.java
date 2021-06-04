da569b6968723d3ba77fb2fa5aaeeffd72699f7e
wzq <1228740123@qq.com>
2021-06-04 17:35:52 +0800

    intersection_of_two_linked_lists

---
+++ src/main/java/com/leetcode/intersection_of_two_linked_lists/Solution1.java
@@ -0,0 +1,31 @@
+/*
+ * Leetcode - intersection_of_two_linked_lists
+ */
+package com.leetcode.intersection_of_two_linked_lists;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+}
---
+++ src/main/java/com/leetcode/intersection_of_two_linked_lists/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - intersection_of_two_linked_lists
+ */
+package com.leetcode.intersection_of_two_linked_lists;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
