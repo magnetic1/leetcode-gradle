730c49de246abffbf86591c2badb2a78261c8f20
wzq <1228740123@qq.com>
2021-10-19 18:21:50 +0800

    design_add_and_search_words_data_structure Solution2

--- src/main/java/com/leetcode/design_add_and_search_words_data_structure/Solution2.java
+++ src/main/java/com/leetcode/design_add_and_search_words_data_structure/Solution2.java
@@ -16,6 +16,73 @@ import org.wzq.leetcode.util.*;
  */
 class Solution2 implements Solution {
 
-    
+    static class WordDictionary {
+        private final Trie root;
+
+        public WordDictionary() {
+            root = new Trie();
+        }
+
+        public void addWord(String word) {
+            root.insert(word);
+        }
+
+        public boolean search(String word) {
+            return dfs(word, 0, root);
+        }
+
+        private boolean dfs(String word, int index, Trie node) {
+            if (index == word.length()) {
+                return node.isEnd();
+            }
+            char ch = word.charAt(index);
+            if (Character.isLetter(ch)) {
+                int childIndex = ch - 'a';
+                Trie child = node.getChildren()[childIndex];
+                if (child != null && dfs(word, index + 1, child)) {
+                    return true;
+                }
+            } else {
+                for (int i = 0; i < 26; i++) {
+                    Trie child = node.getChildren()[i];
+                    if (child != null && dfs(word, index + 1, child)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+
+    static class Trie {
+        private final Trie[] children;
+        private boolean isEnd;
+
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        public Trie[] getChildren() {
+            return children;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
 
 }
