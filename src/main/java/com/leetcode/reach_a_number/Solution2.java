5ea7885353075613ee8ec73eeefaad804568043e
wzq <1228740123@qq.com>
2022-11-04 13:58:43 +0800

    reach_a_number

---
+++ src/main/java/com/leetcode/reach_a_number/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - reach_a_number
+ */
+package com.leetcode.reach_a_number;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
