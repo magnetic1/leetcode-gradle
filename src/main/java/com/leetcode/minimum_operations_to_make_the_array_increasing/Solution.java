8795524f844e0b912325ac8afaf08ec7eabb596e
wzq <1228740123@qq.com>
2022-12-11 16:58:04 +0800

    minimum_operations_to_make_the_array_increasing

---
+++ src/main/java/com/leetcode/minimum_operations_to_make_the_array_increasing/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - minimum_operations_to_make_the_array_increasing
+ */
+package com.leetcode.minimum_operations_to_make_the_array_increasing;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/minimum_operations_to_make_the_array_increasing/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - minimum_operations_to_make_the_array_increasing
+ */
+package com.leetcode.minimum_operations_to_make_the_array_increasing;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
