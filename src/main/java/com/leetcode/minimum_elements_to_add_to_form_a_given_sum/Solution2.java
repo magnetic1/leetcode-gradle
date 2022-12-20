90c455c0e61326b643bf41b03ce28bb0ebedd1d1
wzq <1228740123@qq.com>
2022-12-20 21:45:56 +0800

    minimum_elements_to_add_to_form_a_given_sum

---
+++ src/main/java/com/leetcode/minimum_elements_to_add_to_form_a_given_sum/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - minimum_elements_to_add_to_form_a_given_sum
+ */
+package com.leetcode.minimum_elements_to_add_to_form_a_given_sum;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
