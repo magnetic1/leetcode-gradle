fd371c5ec8fb08b58a82720f62d87355c84e14b2
wzq <1228740123@qq.com>
2022-12-20 17:02:03 +0800

    minimum_limit_of_balls_in_a_bag

---
+++ src/main/java/com/leetcode/minimum_limit_of_balls_in_a_bag/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - minimum_limit_of_balls_in_a_bag
+ */
+package com.leetcode.minimum_limit_of_balls_in_a_bag;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/minimum_limit_of_balls_in_a_bag/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - minimum_limit_of_balls_in_a_bag
+ */
+package com.leetcode.minimum_limit_of_balls_in_a_bag;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
