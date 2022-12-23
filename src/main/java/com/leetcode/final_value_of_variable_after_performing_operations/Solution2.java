ee5b51d475356d985eff587a611fdb3960c444d4
wzq <1228740123@qq.com>
2022-12-23 22:15:57 +0800

    final_value_of_variable_after_performing_operations

---
+++ src/main/java/com/leetcode/final_value_of_variable_after_performing_operations/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - final_value_of_variable_after_performing_operations
+ */
+package com.leetcode.final_value_of_variable_after_performing_operations;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
