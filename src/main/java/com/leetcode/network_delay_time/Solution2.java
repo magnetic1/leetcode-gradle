59fb32111d95cc1dd9b157b565d3af9c8ea8307f
wzq <1228740123@qq.com>
2022-12-25 01:31:04 +0800

    network_delay_time

---
+++ src/main/java/com/leetcode/network_delay_time/Solution2.java
@@ -0,0 +1,21 @@
+/*
+ * Leetcode - network_delay_time
+ */
+package com.leetcode.network_delay_time;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    
+
+}
