134e1e89f9c918c9cdb51e01ce8c3c6a7cf1d8b1
wzq <1228740123@qq.com>
2022-12-24 20:55:31 +0800

    form_array_by_concatenating_subarrays_of_another_array

---
+++ src/main/java/com/leetcode/form_array_by_concatenating_subarrays_of_another_array/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - form_array_by_concatenating_subarrays_of_another_array
+ */
+package com.leetcode.form_array_by_concatenating_subarrays_of_another_array;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
