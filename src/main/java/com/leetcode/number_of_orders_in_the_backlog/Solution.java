0f50252d549c6a7d56010fbc471307cb67e50c5b
wzq <1228740123@qq.com>
2023-01-02 20:44:30 +0800

    number_of_orders_in_the_backlog

---
+++ src/main/java/com/leetcode/number_of_orders_in_the_backlog/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - number_of_orders_in_the_backlog
+ */
+package com.leetcode.number_of_orders_in_the_backlog;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/number_of_orders_in_the_backlog/Solution2.java
@@ -0,0 +1,66 @@
+/*
+ * Leetcode - number_of_orders_in_the_backlog
+ */
+package com.leetcode.number_of_orders_in_the_backlog;
+
+import java.util.*;
+
+import org.wzq.leetcode.util.*;
+
+/**
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+    public int getNumberOfBacklogOrders(int[][] orders) {
+        final int MOD = 1000000007;
+        PriorityQueue<int[]> buyOrders = new PriorityQueue<int[]>((a, b) -> b[0] - a[0]);
+        PriorityQueue<int[]> sellOrders = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        for (int[] order : orders) {
+            int price = order[0], amount = order[1], orderType = order[2];
+            if (orderType == 0) {
+                while (amount > 0 && !sellOrders.isEmpty() && sellOrders.peek()[0] <= price) {
+                    int[] sellOrder = sellOrders.poll();
+                    int sellAmount = Math.min(amount, sellOrder[1]);
+                    amount -= sellAmount;
+                    sellOrder[1] -= sellAmount;
+                    if (sellOrder[1] > 0) {
+                        sellOrders.offer(sellOrder);
+                    }
+                }
+                if (amount > 0) {
+                    buyOrders.offer(new int[]{price, amount});
+                }
+            } else {
+                while (amount > 0 && !buyOrders.isEmpty() && buyOrders.peek()[0] >= price) {
+                    int[] buyOrder = buyOrders.poll();
+                    int buyAmount = Math.min(amount, buyOrder[1]);
+                    amount -= buyAmount;
+                    buyOrder[1] -= buyAmount;
+                    if (buyOrder[1] > 0) {
+                        buyOrders.offer(buyOrder);
+                    }
+                }
+                if (amount > 0) {
+                    sellOrders.offer(new int[]{price, amount});
+                }
+            }
+        }
+        int total = 0;
+        for (PriorityQueue<int[]> pq : Arrays.asList(buyOrders, sellOrders)) {
+            while (!pq.isEmpty()) {
+                int[] order = pq.poll();
+                total = (total + order[1]) % MOD;
+            }
+        }
+        return total;
+    }
+
+
+}
