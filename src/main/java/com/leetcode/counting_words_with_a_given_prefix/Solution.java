7f47d274b88cbb44603aef8b55266fad0bfdf027
wzq <1228740123@qq.com>
2023-01-08 22:03:08 +0800

    counting_words_with_a_given_prefix

---
+++ src/main/java/com/leetcode/counting_words_with_a_given_prefix/Solution.java
@@ -0,0 +1,16 @@
+/*
+ * Leetcode - counting_words_with_a_given_prefix
+ */
+package com.leetcode.counting_words_with_a_given_prefix;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    
+}
---
+++ src/main/java/com/leetcode/counting_words_with_a_given_prefix/Solution1.java
@@ -0,0 +1,29 @@
+/*
+ * Leetcode - counting_words_with_a_given_prefix
+ */
+package com.leetcode.counting_words_with_a_given_prefix;
+import java.util.*;
+import org.wzq.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int prefixCount(String[] words, String pref) {
+        int res = 0;
+        for (String word : words) {
+            if (word.startsWith(pref)) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+}
